4c68d51207db815e713b9da58302999343e8da0a
Seongjin Ahn <sjan137@naver.com>
2021-07-24 13:31:45 +0900

    Update app.js

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -7,6 +7,9 @@ function onLoginSubmit(event) {
     event.preventDefault();
     const userName = loginInput.value;
     loginForm.classList.toggle("hidden");
+    greeting.innerText = "Hello " + userName;
+    greeting.innerText = `Hello ${userName}`; // `(백틱) %{변수명} `(백틱)
+    greeting.classList.toggle("hidden");
     console.log(userName);
 }
 
