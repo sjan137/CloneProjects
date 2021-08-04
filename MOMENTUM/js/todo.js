34d46ff3e510a594e3570b4c156bdddf2e95ed8f
Seongjin Ahn <sjan137@naver.com>
2021-08-04 23:57:51 +0900

    start chap7

--- MOMENTUM/js/todo.js
+++ MOMENTUM/js/todo.js
@@ -1,8 +1,11 @@
 const toDoForm = document.querySelector("#todo-form");
+const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.querySelector("#todo-list");
 
 function handleToDoSubmit() {
-
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value = "";
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
