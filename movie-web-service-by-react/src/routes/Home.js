0ee4ec9dcbf90fc90fe295ac6f44a066dd5f8be7
seongjin <sjan137@naver.com>
2022-05-26 15:52:51 +0900

    to do list 만들어보기

---
+++ movie-web-service-by-react/src/routes/Home.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+
+function Home() {
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async() => {
+        const response = await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+        );
+        const json = await response.json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    }
+    useEffect(() => {
+        getMovies();
+    }, [])
+
+    return <div>
+        {loading ? <h1>Loading...</h1> : movies.map(movie => (
+        <Movie 
+        key={movie.id}  // key는 React.js에서만, map 안에서 컴포넌트들을 render할 때 사용하는 것.
+        coverImg={movie.medium_cover_image} 
+        title={movie.title} 
+        summary={movie.summary} 
+        genres={movie.genres}
+        />))}
+    </div>;
+}
+
+export default Home;
